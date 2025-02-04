ab60a0cb136884a8ae1d9f1e6f2b8a485fe2ccd3
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2025-02-04 09:01:51 +0000

    refactor(get-float-frequency-data): optimize

--- src/get-float-frequency-data.ts
+++ src/get-float-frequency-data.ts
@@ -1,20 +1,30 @@
 import FFT from 'fft.js'
 
+// 优化, 避免重复创建对象.
+let fft = new FFT(2)
+let output = new Float32Array(0)
+
 export function getFloatFrequencyData(input: Float32Array): Float32Array {
   // FFT的输入需要填充至2的幂长度以保证FFT的性能.
   input = padToPowerOfTwo(input)
 
-  const fft = new FFT(input.length)
+  const inputSize = input.length
+  if (fft.size !== inputSize) {
+    fft = new FFT(inputSize)
+  }
 
+  const outputSize = inputSize * 2
   // 输出为复数, 表示为[实部, 虚部], 因此是输入数据的两倍长度.
-  const output = new Float32Array(input.length * 2)
+  if (output.length !== outputSize) {
+    output = new Float32Array(outputSize)
+  }
 
   fft.realTransform(output, input)
 
   // 只有前半部分的输出是有用的, 通过取复数的模长将其转换为实数.
   // 因此输出长度为输入长度的一半.
   const frequencyData = new Float32Array(input.length / 2)
-  for (let i = 0, len = output.length / 2; i < len; i += 2) {
+  for (let i = 0, len = inputSize; i < len; i += 2) {
     frequencyData[i / 2] = Math.hypot(output[i], output[i + 1])
   }
 
